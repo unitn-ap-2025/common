2b2a9d862b446e3791498810a56a53de1a78a6c5
MarcoAdami <smarco.adami@gmail.com>
2025-11-24 18:31:37 +0100

    Add asteroid and rocket implementations

--- app/game/src/components/asteroid.rs
+++ app/game/src/components/asteroid.rs
@@ -1,1 +1,17 @@
-
+/// Represents an asteroid object, instanciable by the orchestrator.
+///
+pub struct Asteroid;
+#[allow(dead_code)]
+impl Asteroid {
+    /// Creates a new, default instance of an [Asteroid].
+    ///
+    /// This method is the basic constructor and does not require any
+    /// specific initial parameters.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of [Asteroid].
+   pub fn new() -> Asteroid {
+       Asteroid
+   }
+}
--- app/game/src/components/rocket.rs
+++ app/game/src/components/rocket.rs
@@ -1,1 +1,24 @@
+use crate::components::energy_cell::EnergyCell;
+/// Represents the rocket in the game, used by the planet. 
+///
 
+#[allow(dead_code)]
+struct Rocket;
+#[allow(dead_code)]
+impl Rocket {
+    /// Creates a new instance of [Rocket].
+    ///
+    /// This method serves as the primary constructor and requires an energy cell
+    /// to initialize the rocket.
+    ///
+    /// # Arguments
+    ///
+    /// * `_energy_cell` - An energy cell ([EnergyCell]) used to build the rocket.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new instance of [Rocket].
+    pub(crate) fn new(_energy_cell: EnergyCell) -> Self {
+        Rocket
+    }
+}
