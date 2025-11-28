946964dc67c77ec4e20bf8e361cc28a0476f2135
JacopoMazza <jacopoandrea.mazza@studenti.unitn.it>
2025-11-28 21:50:54 +0100

    Fix Issue #24 and minor changes to messages.rs (#35)
    * Minor Fixes
    
    * Update messages.rs

--- app/game/src/protocols/messages.rs
+++ app/game/src/protocols/messages.rs
@@ -3,12 +3,12 @@
 //! Defines the types of messages exchanged between the different
 //! components using [mpsc] channels.
 
-use std::sync::{mpsc, Arc};
+use std::sync::{mpsc};
 use std::time::SystemTime;
 use crate::components::asteroid::Asteroid;
 use crate::components::rocket::Rocket;
 use crate::components::sunray::Sunray;
-use crate::components::resource::{BasicResource, BasicResourceType, Combinator, ComplexResource, ComplexResourceType, GenericResource};
+use crate::components::resource::{Bag, BasicResource, BasicResourceType, ComplexResource, ComplexResourceRequest, ComplexResourceType};
 use crate::components::planet::PlanetState;
 
 //placeholder for the BagContentResponse
@@ -23,9 +23,7 @@ pub enum OrchestratorToPlanet {
     Asteroid(Asteroid),
     StartPlanetAI(StartPlanetAiMsg),
     StopPlanetAI(StopPlanetAiMsg),
-    ManualStopPlanetAI(ManualStopPlanetAiMsg),
-    ManualStartPlanetAI(ManualStartPlanetAiMsg),
-    InternalStateRequest(InternalStateRequestMsg),
+    InternalStateRequest(InternalStateRequestMsg), //I think orchestrator should always have the internal state for the UI, but up to discussions
 }
 pub struct StartPlanetAiMsg;
 pub struct StopPlanetAiMsg;
@@ -42,7 +40,7 @@ pub enum PlanetToOrchestrator {
     StopPlanetAIResult { planet_id: u32, timestamp: SystemTime },
     ManualStopPlanetAIResult { planet_id: u32, timestamp: SystemTime },
     ManualStartPlanetAIResult { planet_id: u32, timestamp: SystemTime },
-    InternalStateResponse { planet_id: u32, planet_state: Arc<PlanetState>,  timestamp: SystemTime },
+    InternalStateResponse { planet_id: u32, planet_state: PlanetState,  timestamp: SystemTime }, //do we want to clone the planetState?, orchestrator should always know the planetState
 }
 
 /// Messages sent by the `Orchestrator` to an `Explorer`.
@@ -57,7 +55,7 @@ pub enum OrchestratorToExplorer {
     GenerateResourceRequest(GenerateResourceRequest),
     CombineResourceRequest (CombineResourceRequest),
     BagContentRequest(BagContentRequestMsg),
-    NeighborsResponse{ neighbors: Vec<Arc<PlanetState>> },
+    NeighborsResponse{ neighbors: Vec<u32> }, //do we want to send ids of the planets?
 
 }
 
@@ -73,16 +71,16 @@ pub struct SupportedResourceRequest;
 pub struct SupportedCombinationRequest;
 
 /// Messages sent by an `Explorer` to the `Orchestrator`.
-pub enum ExplorerToOrchestrator {
+pub enum ExplorerToOrchestrator <T> {
     StartExplorerAIResult { explorer_id: u32, timestamp: SystemTime },
     StopExplorerAIResult  { explorer_id: u32, timestamp: SystemTime },
     MovedToPlanetResult  { explorer_id: u32, timestamp: SystemTime },
     CurrentPlanetResult  { explorer_id: u32, planet_id: u32, timestamp: SystemTime },
-    SupportedResourceResult { explorer_id: u32, supported_resources: Vec<BasicResourceType> ,timestamp: SystemTime, },
-    SupportedCombinationResult { explorer_id: u32, combination_list: Option<Arc<Combinator>>, timestamp: SystemTime },
+    SupportedResourceResult { explorer_id: u32, supported_resources: Option<Vec<BasicResourceType>> ,timestamp: SystemTime, },
+    SupportedCombinationResult { explorer_id: u32, combination_list: Option<Vec<ComplexResourceType>>, timestamp: SystemTime },
     GenerateResourceResponse { explorer_id: u32, generated: Result<(), ()> , timestamp: SystemTime }, //tells to the explorer if the asked resource has been generated
     CombineResourceResponse { explorer_id: u32, generated: Result<(), ()> , timestamp: SystemTime },
-    BagContentResponse { explorer_id: u32, bag_content: Arc<ExplorerBag> , timestamp: SystemTime },
+    BagContentResponse { explorer_id: u32, bag_content: Box<dyn Bag<T>> , timestamp: SystemTime },
     NeighborsRequest { explorer_id: u32, current_planet_id: u32, timestamp: SystemTime },
 }
 
@@ -93,7 +91,7 @@ pub enum ExplorerToPlanet {
     SupportedResourceRequest{ explorer_id: u32 },
     SupportedCombinationRequest { explorer_id: u32 },
     GenerateResourceRequest { explorer_id: u32, msg: GenerateResourceRequest },
-    CombineResourceRequest { explorer_id: u32, msg: CombineResourceRequest },
+    CombineResourceRequest { explorer_id: u32, msg: ComplexResourceRequest },
     AvailableEnergyCellRequest { explorer_id: u32 },
     InternalStateRequest { explorer_id: u32 },
 }
@@ -102,21 +100,14 @@ pub struct GenerateResourceRequest {
     #[allow(unused)]
     resource: BasicResourceType,
 }
-pub struct CombineResourceRequest {
-    #[allow(unused)]
-    first: GenericResource,
-    #[allow(unused)]
-    second: GenericResource,
-    #[allow(unused)]
-    to_generate: ComplexResourceType,
-}
+pub struct CombineResourceRequest {} ///TODO delete this line, only use to comply with old code of the orchestrator
 
 /// Messages sent by a `Planet` to an `Explorer`.
 pub enum PlanetToExplorer {
     SupportedResourceResponse { resource_list: Option<Vec<BasicResourceType>> },
-    SupportedCombinationResponse { combination_list: Option<Arc<Combinator>> },
+    SupportedCombinationResponse { combination_list: Option<Vec<ComplexResourceType>> },
     GenerateResourceResponse { resource: Option<BasicResource> },
     CombineResourceResponse { complex_response: Option<ComplexResource> },
     AvailableEnergyCellResponse { available_cells: u32 },
-    InternalStateResponse { planet_state: Arc<PlanetState> },
+    InternalStateResponse { planet_state: PlanetState },
 }
