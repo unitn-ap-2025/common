9f9cddbfa2f90c87080b4101bb5358075bfedbfa
JacopoMazza <jacopoandrea.mazza@studenti.unitn.it>
2025-12-17 14:45:38 +0100

    Run cargo clippy + fmt

--- app/game/src/protocols/orchestrator_explorer.rs
+++ app/game/src/protocols/orchestrator_explorer.rs
@@ -19,37 +19,37 @@ use crate::components::resource::{BasicResource, ComplexResource};
 #[strum_discriminants(name(OrchestratorToExplorerKind))]
 pub enum OrchestratorToExplorer {
     /// This variant is used to start an Explorer AI
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::StartExplorerAIResult`]
-    /// 
+    ///
     /// **Use Case**: Starting the Explorer AI at game start
     StartExplorerAI,
     /// This variant is used to reset the Explorer AI and restart it if it is in manual mode
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::ResetExplorerAIResult`]
-    /// 
+    ///
     /// **Use Case**: Reset the Explorer knowledge or restart the AI if it is in manual mode
     ResetExplorerAI,
     /// This variant is used to kill an Explorer
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::KillExplorerResult`]
-    /// 
+    ///
     /// **Use Case**: Killing the explorer instantly
     KillExplorer,
     ///This variant is used to stop the Explorer AI from autonomous decision-making
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::StopExplorerAIResult`]
-    /// 
+    ///
     /// **Use Case**: Stopping the autonomous decision-making and entering the manual mode
     StopExplorerAI,
     /// This variant is used to tell the Explorer to move to a different planet
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::MovedToPlanetResult`]
-    /// 
+    ///
     /// **Use Case**
-    /// 
+    ///
     /// When in manual mode, the orchestrator moves the explorer to a new planet and gives the new [Sender]
-    /// 
+    ///
     /// When in normal mode, this is the response to [`ExplorerToOrchestrator::TravelToPlanetRequest`], in this case
     /// the orchestrator checks that the explorer can move to the planet specified in the request and sends the optional new sender
     MoveToPlanet {
@@ -57,47 +57,47 @@ pub enum OrchestratorToExplorer {
         sender_to_new_planet: Option<Sender<ExplorerToPlanet>>,
     },
     /// This variant is used to ask the ID of the Planet in which the Explorer is currently located
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::CurrentPlanetResult`]
     CurrentPlanetRequest,
     /// This variant is used to enforce the Explorer to ask the supported Resources on the Planet
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::SupportedResourceResult`]
-    /// 
+    ///
     /// **Use Case**: In manual mode, ask the explorer to send a [`ExplorerToPlanet::SupportedResourceRequest`] to know the available [`BasicResourceType`] on its current planet
     SupportedResourceRequest,
     /// This variant is used to enforce the Explorer to ask the supported Combinations on the Planet
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::SupportedCombinationResult`]
-    /// 
+    ///
     /// **Use Case**: In manual mode, ask the explorer to send a [`ExplorerToPlanet::SupportedCombinationRequest`] to know the available [`ComplexResourceType`] on its current planet
     SupportedCombinationRequest,
     /// This variant is used to enforce the Explorer to ask the Planet to Generate a [`BasicResource`]
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::GenerateResourceResponse`]
-    /// 
+    ///
     /// **Use Case**: In manual mode, ask the explorer to send a [`ExplorerToPlanet::GenerateResourceRequest`] craft a [`BasicResource`]
     GenerateResourceRequest {
         ///The type of basic resource to craft
         to_generate: BasicResourceType,
     },
     /// This variant is used to enforce the Explorer to ask the Planet to Generate a [`ComplexResource`] provided by [`ComplexResourceType`]
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::CombineResourceResponse`]
-    /// 
+    ///
     /// **Use Case**: In manual mode, ask the explorer to send a [`ExplorerToPlanet::CombineResourceRequest`] to craft a [`ComplexResource`]
     CombineResourceRequest {
         ///The type of complex resource to generate
         to_generate: ComplexResourceType,
     },
     /// This variant is used to ask the content of the Explorer Bag
-    /// 
+    ///
     /// **Expected Response**: [`ExplorerToOrchestrator::BagContentResponse`]
-    /// 
+    ///
     /// **Use Case**: Message used by the GUI to get information on the Explorer bag content to be shown
     BagContentRequest,
     /// This variant is used to send to the Explorer the IDs of the planets to which it can be moved
-    /// 
+    ///
     /// **Response To**: [`ExplorerToOrchestrator::NeighborsRequest`]
     NeighborsResponse {
         ///The list of IDs of the planets to which it can be moved
@@ -109,42 +109,42 @@ pub enum OrchestratorToExplorer {
 #[strum_discriminants(name(ExplorerToOrchestratorKind))]
 pub enum ExplorerToOrchestrator<T> {
     /// This variant is used to acknowledge the starting of the Explorer AI
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::StartExplorerAI`]
     StartExplorerAIResult {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
     },
     /// This variant is used to acknowledge the killing of an Explorer
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::KillExplorer`]
     KillExplorerResult {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
     },
     /// This variant is used to acknowledge the reset of the Explorer AI
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::ResetExplorerAI`]
     ResetExplorerAIResult {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
     },
     /// This variant is used to acknowledge the stopping of the Explorer AI
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::StopExplorerAI`]
     StopExplorerAIResult {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
     },
     /// This variant is used to acknowledge the transfer of an Explorer to a new Planet
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::MoveToPlanet`]
     MovedToPlanetResult {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
     },
     /// This variant is used to send the ID of the current planet on which the Explorer is located
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::CurrentPlanetRequest`]
     CurrentPlanetResult {
         ///The ID of the explorer sending the message
@@ -153,7 +153,7 @@ pub enum ExplorerToOrchestrator<T> {
         planet_id: ID,
     },
     /// This variant is used to send the list of the available [`BasicResourceType`] in the Explorer's current planet
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::SupportedResourceRequest`]
     SupportedResourceResult {
         ///The ID of the explorer sending the message
@@ -162,7 +162,7 @@ pub enum ExplorerToOrchestrator<T> {
         supported_resources: HashSet<BasicResourceType>,
     },
     /// This variant is used to send the list of the available [`ComplexResourceType`] in the Explorer's current planet
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::SupportedCombinationRequest`]
     SupportedCombinationResult {
         ///The ID of the explorer sending the message
@@ -171,29 +171,29 @@ pub enum ExplorerToOrchestrator<T> {
         combination_list: HashSet<ComplexResourceType>,
     },
     /// This variant is used to send the generated Basic Resource asked by the Orchestrator
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::GenerateResourceRequest`]
     GenerateResourceResponse {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
         ///A Result consisting of: [Ok] if the requested resource has been generated and added to the Explorer Bag
-        /// 
+        ///
         ///An [Err] String if the requested resource has not been generated
         generated: Result<(), String>,
     },
     /// This variant is used to send the generated [`ComplexResource`] asked by the Orchestrator
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::CombineResourceRequest`]
     CombineResourceResponse {
         ///The ID of the Explorer sending the message
         explorer_id: ID,
         ///A Result consisting of: [Ok] if the requested resource has been generated and added to the Explorer Bag
-        /// 
+        ///
         ///An [Err] String if the requested resource has not been generated
         generated: Result<(), String>,
     },
     /// This message is for passing around the bag content and has been implemented with a generic type to let the group the freedom to implement the methods on it
-    /// 
+    ///
     /// **Response To**: [`OrchestratorToExplorer::BagContentRequest`]
     BagContentResponse {
         ///The ID of the explorer sending the message
@@ -202,9 +202,9 @@ pub enum ExplorerToOrchestrator<T> {
         bag_content: T,
     },
     /// This variant asks the Orchestrator for the list of neighbors Planets to travel to
-    /// 
+    ///
     /// **Expected Response**: [`OrchestratorToExplorer::NeighborsResponse`]
-    /// 
+    ///
     /// **Use Case**: Knowing reachable planets from current planet
     NeighborsRequest {
         ///The ID of the Explorer sending the message
@@ -213,9 +213,9 @@ pub enum ExplorerToOrchestrator<T> {
         current_planet_id: ID,
     },
     /// This variant asks the Orchestrator to be sent to the specified Planet
-    /// 
+    ///
     /// **Expected Response**: [`OrchestratorToExplorer::MoveToPlanet`]
-    /// 
+    ///
     /// **Use Case**: Autonomously asking to travel to a planet
     TravelToPlanetRequest {
         ///The ID of the Explorer sending the message
--- app/game/src/protocols/orchestrator_planet.rs
+++ app/game/src/protocols/orchestrator_planet.rs
@@ -15,11 +15,7 @@ use enum_as_inner::EnumAsInner;
 use strum_macros::EnumDiscriminants;
 
 #[cfg(doc)]
-use {
-    crate::components::energy_cell::EnergyCell,
-    crate::components::planet::Planet
-};
-
+use {crate::components::energy_cell::EnergyCell, crate::components::planet::Planet};
 
 /// This enum describes all possible messages from the Orchestrator to a Planet
 #[derive(Debug, EnumAsInner, EnumDiscriminants)]
@@ -32,40 +28,40 @@ pub enum OrchestratorToPlanet {
     /// **Use Case**: sending a [Sunray] to charge [`EnergyCell`]
     Sunray(Sunray),
     /// This variant is used to send an [Asteroid] to a planet
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::AsteroidAck`]
-    /// 
+    ///
     /// **Use Case**: sending an [Asteroid] to attack a [Planet]
     Asteroid(Asteroid),
     /// This variant is used to start a Planet AI and restart it if it is stopped
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::StartPlanetAIResult`]
-    /// 
+    ///
     /// **Use Case**: Starting the Planet AI at game start or restart the AI in case it is stopped
     StartPlanetAI,
     /// This variant is used to pause the planet Ai
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::StopPlanetAIResult`]
-    /// 
-    /// **Use Case**: Freezing Planet ability to respond to every message, 
+    ///
+    /// **Use Case**: Freezing Planet ability to respond to every message,
     /// a planet in this state will only answer with [`PlanetToOrchestrator::Stopped`]
     StopPlanetAI,
     /// This variant is used to kill (or destroy) the planet
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::KillPlanetResult`]
-    /// 
+    ///
     /// **Use Case**: Instantly kill a Planet
     KillPlanet,
     /// This variant is used to obtain a Planet Internal State
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::InternalStateResponse`]
-    /// 
+    ///
     /// **Use Case**: The GUI can use this message to obtain the relevant info of the planet to be shown
     InternalStateRequest,
     /// This variant is used to advertise an incoming explorer to a planet
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::IncomingExplorerResponse`]
-    /// 
+    ///
     /// **Use Case**: Moving an explorer to this planet
     IncomingExplorerRequest {
         ///The incoming explorer's id
@@ -74,9 +70,9 @@ pub enum OrchestratorToPlanet {
         new_sender: Sender<PlanetToExplorer>,
     },
     /// This variant is used to advertise an outgoing explorer to a planet
-    /// 
+    ///
     /// **Expected Response**: [`PlanetToOrchestrator::OutgoingExplorerResponse`]
-    /// 
+    ///
     /// **Use Case**: Asking the planet to delete the [Sender] to the outgoing explorer
     OutgoingExplorerRequest {
         ///The outgoing explorer's id
@@ -89,7 +85,7 @@ pub enum OrchestratorToPlanet {
 #[strum_discriminants(name(PlanetToOrchestratorKind))]
 pub enum PlanetToOrchestrator {
     /// This variant is used to acknowledge the obtained [Sunray]
-    /// 
+    ///
     /// **Response to**: [`OrchestratorToPlanet::Sunray`]
     SunrayAck {
         ///ID of the planet sending the message
@@ -97,7 +93,7 @@ pub enum PlanetToOrchestrator {
     },
     /// This variant is used to acknowledge the obtained [Asteroid] and notify the orchestrator
     /// if the planet has a rocket to defend itself
-    /// 
+    ///
     /// **Response to**: [`OrchestratorToPlanet::Asteroid`]
     AsteroidAck {
         ///ID of the planet sending the message
@@ -106,7 +102,7 @@ pub enum PlanetToOrchestrator {
         rocket: Option<Rocket>,
     },
     /// This variant is used to acknowledge the starting of the Planet Ai
-    /// 
+    ///
     /// **Response to**: [`OrchestratorToPlanet::StartPlanetAI`]
     StartPlanetAIResult {
         ///ID of the planet sending the message
@@ -114,7 +110,7 @@ pub enum PlanetToOrchestrator {
     },
     /// This variant is used to acknowledge the stopping of the Planet Ai, in this state a planet will only respond
     /// to incoming messages with a [`PlanetToOrchestrator::Stopped`]
-    /// 
+    ///
     /// **Response to**: [`OrchestratorToPlanet::StopPlanetAI`]
     StopPlanetAIResult {
         ///ID of the planet sending the message
@@ -122,11 +118,11 @@ pub enum PlanetToOrchestrator {
     },
     /// This variant is used to acknowledge the killing of a planet, in this case the planet thread will be terminated
     /// and the planet will be deleted from the galaxy
-    /// 
+    ///
     /// **Response to**: [`OrchestratorToPlanet::KillPlanet`]
     KillPlanetResult { planet_id: ID },
     /// This variant is used to send back the Planet State
-    /// 
+    ///
     /// **Response to** [`OrchestratorToPlanet::InternalStateRequest`]
     InternalStateResponse {
         ///ID of the planet sending the message
@@ -135,7 +131,7 @@ pub enum PlanetToOrchestrator {
         planet_state: DummyPlanetState,
     },
     /// This variant is used to acknowledge the incoming explorer reception
-    /// 
+    ///
     /// **Response to** [`OrchestratorToPlanet::IncomingExplorerRequest`]
     IncomingExplorerResponse {
         ///ID of the planet sending the message
@@ -143,14 +139,14 @@ pub enum PlanetToOrchestrator {
         ///Incoming explorer's ID
         explorer_id: ID,
         ///Result of the operation:
-        /// 
+        ///
         /// [Ok] if the [Sender] to the incoming explorer has been correctly set up
-        /// 
+        ///
         /// [Err(String)] if an error occurred
         res: Result<(), String>,
     },
     /// This variant is used to acknowledge that an explorer is leaving the planet
-    /// 
+    ///
     /// **Response to**: [`OrchestratorToPlanet::OutgoingExplorerRequest`]
     OutgoingExplorerResponse {
         ///ID of the planet sending the message
@@ -158,9 +154,9 @@ pub enum PlanetToOrchestrator {
         ///Incoming explorer's ID
         explorer_id: ID,
         ///Result of the operation:
-        /// 
+        ///
         /// [Ok] if the [Sender] to the outgoing explorer has been correctly deleted
-        /// 
+        ///
         /// [Err(String)] if an error occurred
         res: Result<(), String>,
     },
